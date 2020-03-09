ed5291dc3a5957efef66bba246498ffce586118a
Tymoteusz Tula <tymoteusz.tula@gmail.com>
2020-03-09 13:55:59 +0000

    some changes

--- Sources/MonteCarloSampling/monte_carlo.cpp
+++ Sources/MonteCarloSampling/monte_carlo.cpp
@@ -1,5 +1,21 @@
-#include <Eigen/Dense>
+#include <eigen3/Eigen/Dense>
 #include "monte_carlo.hpp"
 
-using Eigen::MatrixXd
+using Eigen::MatrixXd;
 
+MonteCarloSampleGenerator::MonteCarloSampleGenerator(
+    MatrixXd& weights_old[n_l], MatrixXd& weights_new[n_l],
+        MatrixXd& biases_old[n_l], MatrixXd& biases_new[n_l], int no_samples)
+{
+    ns = no_samples;
+    w_old = weights_old;
+    w_new = weights_new;
+    b_old = biases_old;
+    b_new = biases_new;
+    sample_matrix = MatrixXd::Zero(no_samples, w_old[0].rows());
+}
+
+MatrixXd MonteCarloSampleGenerator::GetSamples()
+{
+    return sample_matrix;
+}
--- Sources/MonteCarloSampling/monte_carlo.hpp
+++ Sources/MonteCarloSampling/monte_carlo.hpp
@@ -1,25 +1,32 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
-#include <Eigen/Dense>
+#include <eigen3/Eigen/Dense>
 
-using Eigen::MatrixXd
+using Eigen::MatrixXd;
+
+const int n_l = 5;
 
 class MonteCarloSampleGenerator
 {
 public:
-    MonteCarloSampleGenerator(MatrixXd weights_old, MatrixXd weights_new,
-        MatrixXd biases_old, MatrixXd biases_new);
+    MonteCarloSampleGenerator(MatrixXd& weights_old[n_l], MatrixXd& weights_new[n_l],
+        MatrixXd& biases_old[n_l], MatrixXd& biases_new[n_l], int no_samples);
     MatrixXd GetSamples();
 private:
-    MatrixXd sampleMatrix;
+    MatrixXd sample_matrix;
+    int ns;
+    MatrixXd w_old[n_l];
+    MatrixXd w_new[n_l];
+    MatrixXd b_old[n_l];
+    MatrixXd b_new[n_l];
     void GenerateSamples();
     MatrixXd NetworkOutput();
     MatrixXd MonteCarloStep();
-}
+};
 
 
-PYBIND11_MODULE(monte_carlo, m)
-{
-    m.doc() = "pybind example plugin";
-
-}
+//PYBIND11_MODULE(monte_carlo, m)
+//{
+//    m.doc() = "pybind example plugin";
+//
+//}
